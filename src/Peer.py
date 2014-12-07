33694c42bd4c6baaf0f7e00b4d6cd109e091e1b3
Emily Pakulski <enp2111@columbia.edu>
2014-12-06 22:01:55 -0500

    Added object orientation and multithreaded handling peers.

--- src/Peer.py
+++ src/Peer.py
@@ -11,43 +11,47 @@ Run using 'python Peer.py <local_port_no> <timeout_seconds>
 from socket import socket, AF_INET, SOCK_STREAM
 from sys import argv, stdout, exit
 from threading import Thread
-
-BUFF_SIZE = 4096
-local_IP = '127.0.0.1'
-BACKLOG = 5
-
-# Connects to server socket and starts send and receive threads.
-def main(argv):
-    local_port_no = int(argv[1])
-    timeout_seconds = int(argv[2])
     
-    # maps IPs to tuple of (weight, socket)
-    routing_table = {}
-    
-    # connect to peers passed in as arguments
-    for n in range(3, len(argv) - 2, 3):
-        remote_ip = argv[n]
-        remote_port = argv[n + 1]
-        remote_weight = argv[n + 2]
-        
+class Peer:
+    local_IP = '127.0.0.1'
+    BACKLOG = 5
+
+    def handle_new_peer(self, peer_socket, peer_ip, peer_port):
+        print 'New peer connected on '  + str(peer_ip) + ':' + str(peer_port) + '. '
+        stdout.flush()
+
+    def connect_to_peer(self, remote_ip, remote_port, remote_weight):
         remote_sock = socket(AF_INET, SOCK_STREAM)
         remote_sock.connect((remote_ip,remote_port))
-        
-        routing_table[remote_ip] = (remote_weight, remote_sock)
-        
-        #print argv[n] + ' ' + argv[n + 1] + ' ' + argv[n + 2]
-   
-    # listen for more
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.bind((local_IP, local_port_no))
-    sock.listen(BACKLOG)
 
-    while 1:
-        client_connection, addr = sock.accept()
-        print 'New peer connected on '  + str(addr[0]) + ':' + str(addr[1]) + '. '
-        stdout.flush()
-        #thread = Thread(target=handle_client, args=(client_connection, addr))
-        #thread.start()
+        self.routing_table[remote_ip] = (remote_weight, remote_sock)
+        
+        #print remote_ip + ' ' + remote_port + ' ' + remote_weight
+
+    def __init__(self, argv):
+        local_port_no = int(argv[1])
+        self.timeout_seconds = int(argv[2])
+        
+        # maps each IP to tuple of (weight, socket)
+        self.routing_table = {}
+        
+        # connect to peers passed in as arguments
+        for n in range(3, len(argv) - 2, 3):
+            self.connect_to_peer(argv[n], argv[n + 1], argv[n + 2])      
+       
+        # listen for more
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.bind((self.local_IP, local_port_no))
+        sock.listen(self.BACKLOG)
     
+        while 1:
+            client_connection, addr = sock.accept()
+            
+            thread = Thread(target=self.handle_new_peer, 
+                            args=(client_connection, addr[0], addr[1]))
+            thread.start()
+        
+def main(argv):
+    Peer(argv)
     
 main(argv)
